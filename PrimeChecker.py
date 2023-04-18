5fbece33016be47f75bb623ad634e243510ef5df
Sam Hildebrand <sch_numberone@yahoo.com>
2023-04-18 10:46:39 -0500

    Initial Cypto stuffs

---
+++ PrimeChecker.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+# Large Prime Generation for RSA
+import random
+ 
+# Pre generated primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349] 
+ 
+def isMillerRabinPassed(mrc):
+    '''Run 20 iterations of Rabin Miller Primality test'''
+    maxDivisionsByTwo = 0
+    ec = mrc-1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert(2**maxDivisionsByTwo * ec == mrc-1)
+ 
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+                return False
+        return True
+ 
+    # Set number of trials here
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
+ 
+ 
+if __name__ == '__main__':
+    print("Enter a number to check if it's prime: ", end="")
+    prime_candidate = input()
+    pc = int(prime_candidate)
+
+    if (pc == 1 or pc == 2):
+        print(prime_candidate, "is prime! woo.")
+        exit()
+
+    for divisor in first_primes_list:
+        if (pc % divisor == 0 and divisor**2 <= pc):
+            print(prime_candidate, "is not prime.")
+            exit()    
+
+    if not isMillerRabinPassed(pc):
+        print(prime_candidate, "is not prime.")
+        exit()
+    else:
+        print(prime_candidate, "is prime! woo.")
