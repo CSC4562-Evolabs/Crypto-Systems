36c94ccf43d4550a1fa01ce82c7e07c642c1ba07
Sean Chappell <98842466+seanchappell17@users.noreply.github.com>
2023-04-25 19:24:00 -0500

    Added comments
    Added comments to all the methods that are created, and what their use is/for.

--- ExhaustiveSearch.py
+++ ExhaustiveSearch.py
@@ -6,11 +6,14 @@
 
 import time
 
+# Define a function to find the Greatest Common Divisor (GCD) 
+# of two numbers using the Euclidean algorithm
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
+# Define a function to find the Extended GCD of two numbers
 def extended_gcd(a, b):
     if a == 0:
         return b, 0, 1
@@ -18,6 +21,8 @@ def extended_gcd(a, b):
         g, x, y = extended_gcd(b % a, a)
         return g, y - (b // a) * x, x
 
+# Define a function to find the modular inverse of two 
+# numbers e and phi
 def mod_inverse(e, phi):
     g, x, _ = extended_gcd(e, phi)
     if g != 1:
@@ -25,12 +30,16 @@ def mod_inverse(e, phi):
     else:
         return x % phi
 
+# Define a function to factorize a number n into its two 
+# prime factors
 def factorize_n(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return i, n // i
     return None
 
+# Define a function to find the private key (d) from the 
+# public key (e, n)
 def find_private_key(e, n):
     start_time = time.time()
 
@@ -44,10 +53,11 @@ def find_private_key(e, n):
     time_cost = time.time() - start_time
     return d, p, q, time_cost
 
-# Example public key
 e = 926933
 n = 1185137
 
+# Find the private key (d) and prime factors (p, q) for 
+# the given public key (e, n)
 d, p, q, time_cost = find_private_key(e, n)
 print(f"Private key d: {d}")
 print(f"Prime factors p: {p}, q: {q}")
